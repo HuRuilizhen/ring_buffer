9ffea01167f7a3b7521b6371cbded9818c1c6a18
Ruilizhen Hu <huruilizhen@gmail.com>
2025-06-25 01:41:42 +0800

    fix: revise memory align mechanism (#13)
    * fix: mem align
    
    * fix: add fallback for CACHELINE_SIZE

--- include/ring_buffer/ring_buffer.h
+++ include/ring_buffer/ring_buffer.h
@@ -9,105 +9,73 @@
 
 namespace RingBuffer {
 
+#ifndef __cpp_lib_hardware_interference_size
+constexpr std::size_t CACHELINE_SIZE = 64;  // fallback
+#else
+constexpr std::size_t CACHELINE_SIZE =
+    std::hardware_destructive_interference_size;
+#endif
+
 template <typename T>
-class RingBuffer {
+class alignas(CACHELINE_SIZE) RingBuffer {
  private:
   struct Deleter {
     std::size_t alignment;
-    void operator()(T *ptr) const noexcept {
+    void operator()(T* ptr) const noexcept {
       ::operator delete[](ptr, std::align_val_t(alignment));
     }
   };
 
   std::unique_ptr<T[], Deleter> buffer;
-  size_t capacity;
-  size_t size;
-  size_t head;
-  size_t tail;
-  std::size_t alignment;
+  const size_t capacity;
+  const std::size_t alignment;
+  alignas(CACHELINE_SIZE) size_t size = 0;
+  alignas(CACHELINE_SIZE) size_t head = 0;
+  alignas(CACHELINE_SIZE) size_t tail = 0;
 
  public:
-  explicit RingBuffer(size_t capacity, std::size_t align = alignof(T));
-  ~RingBuffer() = default;
+  explicit RingBuffer(size_t cap, std::size_t align = CACHELINE_SIZE)
+      : capacity(cap), alignment(align), buffer(nullptr, Deleter{align}) {
+    if (cap == 0)
+      throw std::invalid_argument(
+          "capacity of ring buffer must be greater than 0");
+    T* ptr = static_cast<T*>(
+        ::operator new[](cap * sizeof(T), std::align_val_t(align)));
+    buffer.reset(ptr);
+  }
 
-  void push(const T &value);
-  T pop();
+  void push(const T& value) {
+    if (isFull()) throw std::overflow_error("ring buffer is full");
 
-  bool isEmpty() const;
-  bool isFull() const;
-  size_t getSize() const;
-  size_t getCapacity() const;
+    buffer[tail] = value;
+    tail = (tail + 1) % capacity;
+    size++;
+  }
 
-  void print() const;
-  std::span<const T> getElements() const;
+  T pop() {
+    if (isEmpty()) throw std::underflow_error("ring buffer is empty");
+
+    T value = std::move(buffer[head]);
+    head = (head + 1) % capacity;
+    size--;
+
+    return value;
+  }
+
+  bool isFull() const { return size == capacity; }
+  bool isEmpty() const { return size == 0; }
+  size_t getCapacity() const { return capacity; }
+  size_t getSize() const { return size; }
+
+  void print() const {
+    for (size_t i = 0; i < size; i++)
+      std::cout << buffer[(head + i) % capacity] << " ";
+    std::cout << std::endl;
+  }
+
+  std::span<const T> getElements() const {
+    return std::span<const T>(buffer.get(), size);
+  }
 };
-
-template <typename T>
-RingBuffer<T>::RingBuffer(size_t capacity, std::size_t align)
-    : capacity(capacity),
-      size(0),
-      head(0),
-      tail(0),
-      alignment(align),
-      buffer(nullptr, Deleter{align}) {
-  if (capacity == 0)
-    throw std::invalid_argument(
-        "capacity of ring buffer must be greater than 0");
-  T *ptr = static_cast<T *>(
-      ::operator new[](capacity * sizeof(T), std::align_val_t(align)));
-  buffer.reset(ptr);
-}
-
-template <typename T>
-void RingBuffer<T>::push(const T &value) {
-  if (isFull()) throw std::overflow_error("ring buffer is full");
-
-  buffer[tail] = value;
-  tail = (tail + 1) % capacity;
-  size++;
-}
-
-template <typename T>
-T RingBuffer<T>::pop() {
-  if (isEmpty()) throw std::underflow_error("ring buffer is empty");
-
-  T value = std::move(buffer[head]);
-  head = (head + 1) % capacity;
-  size--;
-
-  return value;
-}
-
-template <typename T>
-bool RingBuffer<T>::isFull() const {
-  return size == capacity;
-}
-
-template <typename T>
-bool RingBuffer<T>::isEmpty() const {
-  return size == 0;
-}
-
-template <typename T>
-size_t RingBuffer<T>::getCapacity() const {
-  return capacity;
-}
-
-template <typename T>
-size_t RingBuffer<T>::getSize() const {
-  return size;
-}
-
-template <typename T>
-void RingBuffer<T>::print() const {
-  for (size_t i = 0; i < size; i++)
-    std::cout << buffer[(head + i) % capacity] << " ";
-  std::cout << std::endl;
-}
-
-template <typename T>
-std::span<const T> RingBuffer<T>::getElements() const {
-  return std::span<const T>(buffer.get(), size);
-}
 
 }  // namespace RingBuffer
